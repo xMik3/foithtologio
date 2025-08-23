3ad36a680f48ec3f98bdc91640d3b229a69def52
Michael Ieronymidis <ieronymidismichael@gmail.com>
2025-08-24 00:36:51 +0300

    basic login page

--- frontend/src/main/java/views/general/Login.java
+++ frontend/src/main/java/views/general/Login.java
@@ -1,148 +1,92 @@
+package views.general;
 
 import javax.swing.*;
 import java.awt.*;
+import org.jdesktop.swingx.prompt.PromptSupport;
 
 public class Login extends JFrame{
-    private JButton lgn;
-    private JTextField usrnm;
-    private JTextField pswd;
-    private JLabel Lusrnm;
-    private JLabel Lpswd;
-    private JPanel p;
+
     private JLabel icon;
-    private JPanel exp1;
-    private JPanel exp2;
-    private JPanel exp3;
-    private JPanel exp4;
-    private JPanel expi;
-    private JPanel expil;
-    private JPanel expir;
-    
-    
-    
-    
-        public Login(String title){
-            super(title);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
-            setLayout(new BorderLayout());
-            
-            p = new JPanel();
-            p.setLayout(new GridBagLayout());
-            
-            lgn = new JButton("Log in");
-            usrnm = new JTextField(20);
-            pswd = new JTextField(20);
-            Lusrnm = new JLabel("Username:");
-            Lpswd = new JLabel("Password:");
-            icon = new JLabel("icon");
-            exp1 = new JPanel();
-            exp2 = new JPanel();
-            exp3 = new JPanel();
-            exp4 = new JPanel();
-            expi = new JPanel();
-            expil = new JPanel();
-            expir = new JPanel();
-            
-            Lpswd.setFont(new Font("Consolas", Font.BOLD, 20));
-            Lpswd.setHorizontalAlignment(SwingConstants.CENTER);
-            Lusrnm.setFont(new Font("Consolas", Font.BOLD, 20));
-            Lusrnm.setHorizontalAlignment(SwingConstants.CENTER);
-            
-            lgn.setPreferredSize(new Dimension(120, 30));
-            
-            GridBagConstraints huh = new GridBagConstraints();
-            huh.fill = GridBagConstraints.NONE;
-            huh.insets=new Insets(10,5,10,5);
-            huh.weighty =1.0;
-            huh.weightx = 1.0;
-            
-            
-            huh.gridx = 0;
-            huh.gridy=0;
-            huh.weightx=0.33;
-            huh.weighty = 0.33;
-             
-            huh.gridx = 1;
-            huh.weightx = 0.33;
-            huh.weighty = 0.33;
-            huh.anchor = GridBagConstraints.CENTER;
-            p.add(icon,huh);
-            
-            huh.gridx = 2;
-            huh.weightx=0.33;
-            huh.weighty = 0.33;
-            
-            huh.gridy = 1;
-            huh.gridx = 0;
-            huh.weightx = 0.33;
-            huh.weighty = 0.16;
-            huh.anchor = GridBagConstraints.NORTHWEST;
-            p.add(Lusrnm,huh);
-            
-            huh.gridy = 2;
-            huh.weightx = 0.33;
-            huh.weighty = 0.16;
-            huh.anchor = GridBagConstraints.NORTHWEST;
-            p.add(Lpswd,huh);
-            
-            huh.gridy=3;
-            huh.weightx=0.33;
-            huh.weighty = 0.33;
-            
-            huh.gridy = 1;
-            huh.gridx = 1;
-            huh.weightx = 0.33;
-            huh.weighty = 0.16;
-            huh.anchor = GridBagConstraints.NORTHWEST;
-            p.add(usrnm,huh);
-            
-            huh.gridy = 2;
-            huh.weightx = 0.33;
-            huh.weighty = 0.16;
-            huh.anchor = GridBagConstraints.NORTHWEST;
-            p.add(pswd,huh);
-            
-            huh.gridy = 3;
-            huh.weightx = 0.33;
-            huh.weighty = 0.33;
-            huh.anchor = GridBagConstraints.NORTHEAST;
-            p.add(lgn,huh);
-            
-            huh.gridx = 2;
-            huh.gridy = 0;
-            huh.weightx=0.33;
-            huh.weighty = 0.33;
-            p.add(exp1,huh);
-            
-            huh.gridy = 1;
-            huh.weightx=0.33;
-            huh.weighty = 0.16;
-            p.add(exp2,huh);
-            
-            huh.gridy = 2;
-            huh.weightx=0.33;
-            huh.weighty = 0.16;
-            p.add(exp3,huh);
-            
-            huh.gridy = 3;
-            huh.weightx=0.33;
-            huh.weighty = 0.33;
-            p.add(exp4,huh);
-            
-            add(p);
-            setSize(500,700);
-            setVisible(true);
-            p.setVisible(true);
-            
-            
-        } 
-        
-        public static void main(String[] args) {
-        new Login("bruh");
-        
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JTextField organizationURLField;
+    private JButton loginButton;
+
+    public Login(String title){
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
+            UIManager.put("TextComponent.arc", 25);
+            UIManager.put( "Button.arc", 999 );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        icon = new JLabel(new ImageIcon(getClass().getResource("/images/temporaryIcon.png")));
+        organizationURLField = new JTextField();
+        organizationURLField.setFont(new Font("Arial", Font.PLAIN, 25));
+        PromptSupport.setPrompt("Enter Organization URL", organizationURLField);
+
+        JPanel leftUpPanel = new JPanel(new BorderLayout());
+        leftUpPanel.add(icon,BorderLayout.CENTER);
+        leftUpPanel.setBorder(BorderFactory.createEmptyBorder(40, 0, 0, 0));
+
+        JPanel leftDownPanel = new JPanel(new BorderLayout());
+        leftDownPanel.add(organizationURLField,BorderLayout.CENTER);
+        leftDownPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 200, 0));
+
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        leftPanel.setLayout(new GridLayout(2,1,0,25));
+        leftPanel.add(leftUpPanel, BorderLayout.CENTER);
+        leftPanel.add(leftDownPanel, BorderLayout.SOUTH);
+
+        usernameField = new JTextField();
+        usernameField.setFont(new Font("Arial", Font.PLAIN, 25));
+        PromptSupport.setPrompt("Enter Username", usernameField);
+
+        passwordField = new JPasswordField();
+        passwordField.setFont(new Font("Arial", Font.PLAIN, 25));
+        PromptSupport.setPrompt("Enter Password", passwordField);
+
+        loginButton = new JButton();
+        loginButton.setFont(new Font("Arial", Font.PLAIN, 22));
+        loginButton.setText("Login");
+
+        JPanel rightUpPanel = new JPanel(new BorderLayout());
+        rightUpPanel.setLayout(new GridLayout(2,1,0,25));
+        rightUpPanel.add(usernameField, BorderLayout.NORTH);
+        rightUpPanel.add(passwordField, BorderLayout.SOUTH);
+        rightUpPanel.setBorder(BorderFactory.createEmptyBorder(100, 0, 55, 0));
+
+        JPanel rightDownPanel = new JPanel(new BorderLayout());
+        rightDownPanel.add(loginButton);
+        rightDownPanel.setBorder(BorderFactory.createEmptyBorder(0, 140, 230, 140));
+
+        JPanel rightPanel = new JPanel(new  BorderLayout());
+        rightPanel.setLayout(new GridLayout(2,1,0,0));
+        rightPanel.add(rightUpPanel, BorderLayout.CENTER);
+        rightPanel.add(rightDownPanel, BorderLayout.SOUTH);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 0, 100));
+        panel.setLayout(new GridLayout(1,2,100,0));
+        panel.add(leftPanel, BorderLayout.WEST);
+        panel.add(rightPanel,BorderLayout.EAST);
+
+        add(panel);
+        setSize(1200, 700);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
     }
+        
+    public static void main(String[] args) {
+        new Login("Login");
 
-
+    }
 
 }
